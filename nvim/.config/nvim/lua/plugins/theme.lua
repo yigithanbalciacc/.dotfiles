34289b083c6d885681d19cd2373588dbef7750d0
yigithanbalciacc <yigithan.balci@accenture.com>
2025-02-17 12:31:31 +0300

    add keymaps and update theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -13,22 +13,26 @@ return {
   --     return opts
   --   end,
   -- },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   name = "gruvbox-material",
+  --   opts = function(_, opts)
+  --     if vim.fn.has("termguicolors") == 1 then
+  --       vim.opt.termguicolors = true
+  --     end
+  --
+  --     vim.opt.background = "dark"
+  --
+  --     vim.g.gruvbox_material_background = "medium"
+  --
+  --     vim.g.gruvbox_material_better_performance = 1
+  --
+  --     vim.cmd([[colorscheme gruvbox-material]])
+  --     return opts
+  --   end,
+  -- },
   {
-    "sainnhe/gruvbox-material",
-    name = "gruvbox-material",
-    opts = function(_, opts)
-      if vim.fn.has("termguicolors") == 1 then
-        vim.opt.termguicolors = true
-      end
-
-      vim.opt.background = "dark"
-
-      vim.g.gruvbox_material_background = "medium"
-
-      vim.g.gruvbox_material_better_performance = 1
-
-      vim.cmd([[colorscheme gruvbox-material]])
-      return opts
-    end,
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
   },
 }
